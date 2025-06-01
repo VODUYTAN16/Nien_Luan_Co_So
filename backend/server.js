e5d1b4f587923098189a0d28306f7ad8f0c60776
Vo Duy Tan <voduytan2004@gmail.com>
2025-06-01 18:46:37 +0700

    change port

--- backend/server.js
+++ backend/server.js
@@ -2050,6 +2050,8 @@ app.put('/api/delete_tourService', (req, res) => {
 // Cấu hình để có thể truy cập ảnh từ thư mục uploads
 app.use('/uploads', express.static('uploads'));
 // Khởi động server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(process.env.MYSQLPORT, () => {
+  console.log(
+    `Server running on http://${process.env.MYSQLHOST}:${process.env.MYSQLPORT}`
+  );
 });

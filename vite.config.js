a24844da2246bddf82e701ef4f0292d862f57749
Vo Duy Tan <voduytan2004@gmail.com>
2025-06-01 20:40:11 +0700

    ppppppp

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,14 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
-import dotenv from 'dotenv';
-dotenv.config();
+
 // https://vite.dev/config/
 export default defineConfig({
   base: '/', // <--- Thêm dòng này
   server: {
     proxy: {
       '/api': {
-        target: `http://${process.env.MYSQLHOST}:${process.env.PORT}`,
+        target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path,
